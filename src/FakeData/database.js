1ecca3bac938a41bcbb5b3701df457ad4431f831
MdAtikBhuiyan <touhidatik81@gmail.com>
2021-06-04 21:22:00 +0600

    ok

--- src/FakeData/database.js
+++ src/FakeData/database.js
@@ -8,21 +8,21 @@ export const foodList = [
   {
     id: '1',
     name: "Meat Pizza",
-    price: 8.30,
+    price: 8.20,
     img: "https://i.ibb.co/nQpdZkd/meat-Pizza.png",
     category: "pizza"
   },
   {
     id: '2',
     name: "Chicken Burger",
-    price: 10,
+    price: 12,
     img: "https://i.ibb.co/0Q1Hn8F/chicken-Burger.png",
     category: "burger"
   },
   {
     id: '3',
     name: "Cheese Pizza",
-    price: 12.30,
+    price: 12.50,
     img: "https://i.ibb.co/92VC25k/cheese-Pizza.png",
     category: "pizza"
   },
@@ -38,28 +38,28 @@ export const cartList = [
   {
     id: '1',
     name: "Meat Pizza",
-    price: 8.30,
+    price: 9.30,
     img: "https://i.ibb.co/nQpdZkd/meat-Pizza.png",
     quantity: 1
   },
   {
     id: '2',
     name: "Chicken Burger",
-    price: 10,
+    price: 12,
     img: "https://i.ibb.co/0Q1Hn8F/chicken-Burger.png",
     quantity: 1
   },
   {
     id: '3',
     name: "Cheese Pizza",
-    price: 12.30,
+    price: 10.30,
     img: "https://i.ibb.co/92VC25k/cheese-Pizza.png",
     quantity: 1
   },
   {
     id: '4',
     name: "Sushi Makizushi",
-    price: 7.00,
+    price: 6.00,
     img: "https://i.ibb.co/L9mtQCG/sushi-Makizushi.png",
     quantity: 1
   }
