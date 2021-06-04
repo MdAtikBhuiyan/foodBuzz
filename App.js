1364483d275e035d7ce3a7a685e19b1b1bca58c7
MdAtikBhuiyan <touhidatik81@gmail.com>
2021-06-04 21:38:34 +0600

    ok

--- App.js
+++ App.js
@@ -9,21 +9,29 @@ export const stateContext = createContext();
 const Stack = createStackNavigator();
 
 const App = () => {
+
   const [food, setFood] = useState([]);
   const [cart, setCart] = useState([]);
+
   return (
+
     <stateContext.Provider value={{
       cartState: [cart, setCart],
       foodState: [food, setFood]
     }}>
+
       <NavigationContainer>
+
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="GetStarted" component={GetStarted} />
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Cart" component={Cart} />
         </Stack.Navigator>
+        
       </NavigationContainer>
+
     </stateContext.Provider>
+
   );
 };
 
